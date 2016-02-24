766a8f36c9c40fb4f26e6295c0e80134fca3f35c
jac32 <jac32@st-andrews.ac.uk>
2016-02-24 01:54:27 +0000

    Added bool encode/decode to demo file

--- Code/demo.py
+++ Code/demo.py
@@ -10,9 +10,15 @@ ParseOMstring(s)
 s = '<OMOBJ> <OMA> <OMS cd="list1" name="list"/> <OMI>41</OMI> <OMI>42</OMI> <OMI>43</OMI> <OMA> <OMS cd="list1" name="list"/> <OMI>41</OMI> <OMI>42</OMI> <OMI>43</OMI> </OMA> </OMA> </OMOBJ>'
 ParseOMstring(s)
 
+s= '<OMOBJ> <OMA> <OMS cd="logic1" name="false"/> </OMA> </OMOBJ>'
+ParseOMstring(s)
+
+
 ParseOMfile('tst/integer.xml')
 ParseOMfile('tst/list.xml')
 ParseOMfile('tst/listnested.xml')
+ParseOMfile('tst/bool.xml')
+OMprint(False)
 OMstring(42)
 OMprint(42)
 
